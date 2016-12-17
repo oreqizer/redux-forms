5806b4b23f982949d625efc778b7fcb83cc3cdfc
oreqizer <oreqizer@gmail.com>
2016-12-17 14:01:02 +0100

    example: Add FieldArray

--- example/index.jsx
+++ example/index.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { reducer } from 'redux-forms';
+import { reducer } from '../lib';
 import createLogger from 'redux-logger';
 import whyDidYouUpdate from 'why-did-you-update';
 
 import Form from './src/Form';
-import Form2 from './src/FlatForm';
+import FlatForm from './src/FlatForm';
 
 whyDidYouUpdate(React);
+
 
 const logger = createLogger({ collapsed: true });
 const store = createStore(combineReducers({
@@ -20,6 +21,7 @@ const Root = () => (
   <Provider store={store}>
     <div>
       <Form />
+      <FlatForm />
     </div>
   </Provider>
 );
