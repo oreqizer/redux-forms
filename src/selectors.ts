3acda880a9668d44030acf4aec8fa013caa86963
oreqizer <oreqizer@gmail.com>
2016-12-29 15:06:44 +0100

    selectors: Add map and field selectors

---
+++ src/selectors.ts
@@ -0,0 +1,28 @@
+import { unflatten } from 'flat';
+import * as R from 'ramda';
+
+import { State } from "./formsDuck";
+import { FormObj, FieldObj } from "./utils/containers";
+
+
+export interface IState {
+  reduxFormLite: State;
+}
+
+export type Mapper = (field: FieldObj) => any;
+
+
+const memUnflat = R.memoize(unflatten);
+
+export const fieldSelector = (name: string, state: IState): Object => {
+  const form = state.reduxFormLite[name];
+
+  return memUnflat(form.fields);
+};
+
+export const mapSelector = (name: string, fn: Mapper, state: IState): Object => {
+  const form = state.reduxFormLite[name];
+
+  // TS doesn't recognize objects as Functors
+  return memUnflat(R.map(fn, <any> form.fields));
+};
