13225a9ef2158f92002056860590daed0d3ce313
oreqizer <oreqizer@gmail.com>
2017-01-02 21:08:07 +0100

    index: Export selectors/actions 2

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,10 @@ import Field from './Field';
 import FieldArray from './FieldArray';
 import reducer from './formsReducer';
 
-import * as selectors from './selectors';
-import * as actions from './actions';
-
 
 export {
   Form,
   Field,
   FieldArray,
   reducer,
-  selectors,
-  actions,
 };
