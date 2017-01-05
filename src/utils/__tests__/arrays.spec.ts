4e8bd59f3094981090a03eecceed3d150783d0e1
oreqizer <oreqizer@gmail.com>
2017-01-05 22:03:05 +0100

    arrays: Add 'move'

--- src/utils/__tests__/arrays.spec.ts
+++ src/utils/__tests__/arrays.spec.ts
@@ -1,4 +1,4 @@
-import { arrayUnshift, arrayShift, arraySwap } from '../arrays';
+import { arrayUnshift, arrayShift, arraySwap, arrayMove } from '../arrays';
 
 import { field } from '../containers';
 
@@ -7,11 +7,14 @@ const field0 = { ...field, value: '0' };
 const field1 = { ...field, value: '1' };
 const field2 = { ...field, value: '2' };
 const field3 = { ...field, value: '3' };
+const field4 = { ...field, value: '4' };
 
 const fields = {
   'flat.0': field0,
   'flat.1': field1,
   'flat.2': field2,
+  'flat.3': field3,
+  'flat.4': field4,
   'medium.0.nest.0': field0,
   'medium.0.nest.1': field1,
   'medium.0.nest.2': field2,
@@ -34,6 +37,8 @@ describe('#arrays', () => {
 
     expect(res['flat.0']).toBe(field1);
     expect(res['flat.1']).toBe(field2);
+    expect(res['flat.2']).toBe(field3);
+    expect(res['flat.3']).toBe(field4);
   });
 
   it('should shift flat array', () => {
@@ -44,15 +49,19 @@ describe('#arrays', () => {
     expect(res['flat.0']).toBe(field0);
     expect(res['flat.2']).toBe(field1);
     expect(res['flat.3']).toBe(field2);
+    expect(res['flat.4']).toBe(field3);
+    expect(res['flat.5']).toBe(field4);
   });
 
   it('should shift flat array - negative', () => {
     const res = arrayShift('flat', 1)(fields);
 
-    expect(res['flat.2']).toBeUndefined();
+    expect(res['flat.4']).toBeUndefined();
 
     expect(res['flat.0']).toBe(field0);
     expect(res['flat.1']).toBe(field2);
+    expect(res['flat.2']).toBe(field3);
+    expect(res['flat.3']).toBe(field4);
   });
 
   it('should shift nested array', () => {
@@ -138,5 +147,35 @@ describe('#arrays', () => {
     expect(res['medium.1.nest.1']).toBe(fields['medium.0.nest.1']);
     expect(res['medium.1.nest.2']).toBe(fields['medium.0.nest.2']);
     expect(res['medium.1.nest.3']).toBe(fields['medium.0.nest.3']);
+  });
+
+  it('should move a field - start', () => {
+    const res = arrayMove('flat', 0, 2)(fields);
+
+    expect(res['flat.0']).toBe(field1);
+    expect(res['flat.1']).toBe(field2);
+    expect(res['flat.2']).toBe(field0);
+    expect(res['flat.3']).toBe(field3);
+    expect(res['flat.4']).toBe(field4);
+  });
+
+  it('should move a field - end', () => {
+    const res = arrayMove('flat', 1, 4)(fields);
+
+    expect(res['flat.0']).toBe(field0);
+    expect(res['flat.1']).toBe(field2);
+    expect(res['flat.2']).toBe(field3);
+    expect(res['flat.3']).toBe(field4);
+    expect(res['flat.4']).toBe(field1);
+  });
+
+  it('should move a field - less', () => {
+    const res = arrayMove('flat', 3, 1)(fields);
+
+    expect(res['flat.0']).toBe(field0);
+    expect(res['flat.1']).toBe(field3);
+    expect(res['flat.2']).toBe(field1);
+    expect(res['flat.3']).toBe(field2);
+    expect(res['flat.4']).toBe(field4);
   });
 });
--- src/utils/arrays.ts
+++ src/utils/arrays.ts
@@ -54,23 +54,28 @@ export function arrayShift(path: string, start: number) {
   }, {}, R.keys(fields));
 }
 
+
+function hasPaths(pos1: string, pos2: string, fields: Fields) {
+  const keys = R.keys(fields);
+  const ok1 = R.compose(
+    R.any(R.identity),
+    R.map<string, boolean>((key) => key.indexOf(pos1) === 0),
+  )(keys);
+
+  const ok2 = R.compose(
+    R.any(R.identity),
+    R.map<string, boolean>((key) => key.indexOf(pos2) === 0),
+  )(keys);
+
+  return ok1 && ok2;
+}
+
 export function arraySwap(path: string, index1: number, index2: number) {
   return (fields: Fields): Fields => {
     const pos1 = `${path}.${index1}`;
     const pos2 = `${path}.${index2}`;
 
-    const keys = R.keys(fields);
-    const ok1 = R.compose(
-      R.any(R.identity),
-      R.map<string, boolean>((key) => key.indexOf(pos1) === 0),
-    )(keys);
-
-    const ok2 = R.compose(
-      R.any(R.identity),
-      R.map<string, boolean>((key) => key.indexOf(pos2) === 0),
-    )(keys);
-
-    if (!ok1 || !ok2) {
+    if (!hasPaths(pos1, pos2, fields)) {
       return fields;
     }
 
@@ -88,36 +93,19 @@ export function arraySwap(path: string, index1: number, index2: number) {
   };
 }
 
-export function arrayMove(path: string, index1: string, index2: string) {
+export function arrayMove(path: string, index1: number, index2: number) {
   return (fields: Fields): Fields => {
     const pos1 = `${path}.${index1}`;
     const pos2 = `${path}.${index2}`;
 
-    const keys = R.keys(fields);
-    const ok1 = R.compose(
-      R.any(R.identity),
-      R.map<string, boolean>((key) => key.indexOf(pos1) === 0),
-    )(keys);
-
-    const ok2 = R.compose(
-      R.any(R.identity),
-      R.map<string, boolean>((key) => key.indexOf(pos2) === 0),
-    )(keys);
-
-    if (!ok1 || !ok2) {
+    if (!hasPaths(pos1, pos2, fields)) {
       return fields;
     }
 
-    return R.reduce((acc, key) => {
-      if (key.indexOf(pos1) === 0) {
-        return R.assoc(R.replace(pos1, pos2, key), R.prop(key, fields), acc);
-      }
-
-      if (key.indexOf(pos2) === 0) {
-        return R.assoc(R.replace(pos2, pos1, key), R.prop(key, fields), acc);
-      }
-
-      return R.assoc(key, R.prop(key, fields), acc);
-    }, {}, R.keys(fields));
+    return R.compose<Fields, Fields, Fields, Fields>(
+      R.assoc(pos2, R.prop(pos1, fields)),
+      arrayUnshift(path, index2),
+      arrayShift(path, index1),
+    )(fields);
   };
 }
